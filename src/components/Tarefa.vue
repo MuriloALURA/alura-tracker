4b7dc9700189ee2987d479b1b7818efd535650ca
Murilo <murilo.alvess@yahoo.com>
2022-08-30 14:01:30 -0300

    feat: 🛠 organizando store e criando modal

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,7 +1,12 @@
 <template>
   <Box>
-    <div class="columns">
-      <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+    <div class="columns clicavel" @click="tarefaClicada">
+      <div class="column is-4">
+        {{ tarefa.descricao || 'Tarefa sem descrição' }}
+      </div>
+      <div class="column is-3">
+        {{ tarefa.projeto?.nome || 'N/D' }}
+      </div>
       <div class="column">
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
       </div>
@@ -10,13 +15,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from 'vue';
 import Cronometro from "./Cronometro.vue";
-import ITarefa from '../interfaces/ITarefa'
-import Box from './Box.vue'
+import Box from "./Box.vue";
+import ITarefa from "../interfaces/ITarefa";
 
 export default defineComponent({
-  name: "Tarefa",
+  name: 'Tarefa',
+  emits: ['aoTarefaClicada'],
   components: {
     Cronometro,
     Box
@@ -26,6 +32,25 @@ export default defineComponent({
       type: Object as PropType<ITarefa>,
       required: true
     }
+  },
+  setup(props, { emit }) {
+    const tarefaClicada = () : void => {
+      emit('aoTarefaClicada', props.tarefa)
+    }
+    const tempoGasto = computed(() => {
+      return new Date(props.tarefa.duracaoEmSegundos * 1000)
+        .toISOString()
+        .substr(11, 8)
+    })
+    return {
+      tarefaClicada,
+      tempoGasto
+    }
   }
 });
 </script>
+<style scoped>
+.clicavel {
+  cursor: pointer;
+}
+</style>
